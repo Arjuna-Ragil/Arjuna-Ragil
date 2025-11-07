19465511b526ec5f8e11f37ace0164b20bdd344b
Arjuna <arjunaragilputera@gmail.com>
2025-11-07 15:01:09 +0700

    feat: info and responsive done

--- src/app/(main)/component/projects.js
+++ src/app/(main)/component/projects.js
@@ -60,17 +60,15 @@ export default function Projects({type}){
         return(
             <div className="min-h-screen h-full w-full flex items-center justify-center md:grid grid-cols-2 p-10" id="saturn">
                 <div className="flex text-white flex-col gap-5 self-center items-center justify-center bg-gray-400/10 border border-white backdrop-blur-sm rounded-2xl p-10">
-                    <h1 className="text-3xl font-bold text-center">Demokratos</h1>
-                    <div className="h-full w-full grid grid-cols-2">
-                        <div className="h-30 w-auto aspect-square relative flex items-center justify-center">
-                            <Image src={"/project/demoLogo.svg"} alt="Logo" fill/>
-                        </div>
-                        <div className="h-30 w-auto aspect-video relative flex items-center justify-center">
-                            <Image src={"/project/demoDoc.svg"} alt="Logo" fill/>
-                        </div>
+                    <div className="flex flex-row items-center justify-center gap-3">
+                        <h1 className="text-3xl font-bold text-center">Demokratos</h1>
+                        <Image src={"/project/demoLogo.svg"} alt="Logo" height={50} width={50}/>
                     </div>
-                    <p className="text-xl text-justify">A web-based platform powered by Gemini AI that helps governments analyze public needs and aspirations.</p>
-                    <div className="h-full w-full grid md:grid-cols-3 grid-cols-2 gap-3 text-black">
+                    <div className="md:h-50 h-30 w-auto aspect-video relative flex items-center justify-center">
+                        <Image src={"/project/demoDoc.svg"} alt="Logo" fill/>
+                    </div>
+                    <p className="text-xl md:text-justify">A web-based platform powered by Gemini AI that helps governments analyze public needs and aspirations.</p>
+                    <div className="h-full w-full grid md:grid-cols-3 grid-cols-2 gap-3">
                         {demokratos.map((tech) => (
                             <div key={tech.name} className="h-full w-full flex flex-col items-center justify-center bg-white/30 rounded-4xl p-3">
                                 <div className="h-full w-auto aspect-square relative flex items-center justify-center">
@@ -93,17 +91,15 @@ export default function Projects({type}){
         return(
             <div className="min-h-screen h-full w-full flex items-center justify-center md:grid grid-cols-2 p-10">
                 <div className="col-start-2 flex text-white flex-col gap-5 self-center items-center justify-center bg-gray-400/10 border border-white backdrop-blur-sm rounded-2xl p-10">
-                    <h1 className="text-3xl font-bold text-center">Planix</h1>
-                    <div className="h-1/2 w-full grid grid-cols-2">
-                        <div className="h-full w-auto aspect-square relative">
-                            <Image src={"/project/planLogo.svg"} alt="Logo" fill/>
-                        </div>
-                        <div className="h-full w-auto aspect-square relative">
-                            <Image src={"/project/planDoc.svg"} alt="Logo" fill/>
-                        </div>
+                    <div className="flex flex-row items-center justify-center gap-3">
+                        <h1 className="text-3xl font-bold text-center">Planix</h1>
+                        <Image src={"/project/planLogo.svg"} alt="Logo" height={50} width={50}/>
                     </div>
-                    <p className="text-xl text-justify">A web-based application that leverages AI (Gemini) and real-time spatial data to support urban planning and land-use analysis.</p>
-                    <div className="h-full w-full grid md:grid-cols-4 grid-cols-2 gap-3 text-black">
+                    <div className="md:h-50 h-30 w-auto aspect-video relative flex items-center justify-center">
+                        <Image src={"/project/planDoc.svg"} alt="Logo" fill/>
+                    </div>
+                    <p className="text-xl md:text-justify">A web-based application that leverages AI (Gemini) and real-time spatial data to support urban planning and land-use analysis.</p>
+                    <div className="h-full w-full grid md:grid-cols-4 grid-cols-2 gap-3">
                         {planix.map((tech) => (
                             <div key={tech.name} className="h-full w-full flex flex-col items-center justify-center bg-white/30 rounded-4xl p-3">
                                 <div className="h-full w-auto aspect-square relative flex items-center justify-center">
@@ -126,17 +122,15 @@ export default function Projects({type}){
         return(
             <div className="min-h-screen h-full w-full flex items-center justify-center md:grid grid-cols-2 p-10">
                 <div className="flex text-white flex-col gap-5 self-center items-center justify-center bg-gray-400/10 border border-white backdrop-blur-sm rounded-2xl p-10">
-                    <h1 className="text-3xl font-bold text-center">PicPrice</h1>
-                    <div className="h-1/2 w-full grid grid-cols-2">
-                        <div className="h-full w-auto aspect-square relative">
-                            <Image src={"/project/ppLogo.svg"} alt="Logo" fill/>
-                        </div>
-                        <div className="h-full w-auto aspect-square relative">
-                            <Image src={"/project/ppDoc.svg"} alt="Logo" fill/>
-                        </div>
+                    <div className="flex flex-row items-center justify-center gap-3">
+                        <h1 className="text-3xl font-bold text-center">PicPrice</h1>
+                        <Image src={"/project/ppLogo.svg"} alt="Logo" height={50} width={50}/>
                     </div>
-                    <p className="text-xl text-justify">AI-powered web application that allows users to upload or take a picture of a product and instantly search for its average price across multiple online platforms.</p>
-                    <div className="h-full w-full grid md:grid-cols-4 grid-cols-2 gap-3 text-black">
+                    <div className="md:h-50 h-30 w-auto aspect-video relative flex items-center justify-center">
+                        <Image src={"/project/ppDoc.svg"} alt="Logo" fill/>
+                    </div>
+                    <p className="text-xl md:text-justify">AI-powered web application that allows users to upload or take a picture of a product and instantly search for its average price across multiple online platforms.</p>
+                    <div className="h-full w-full grid md:grid-cols-4 grid-cols-2 gap-3">
                         {picprice.map((tech) => (
                             <div key={tech.name} className="h-full w-full flex flex-col items-center justify-center bg-white/30 rounded-4xl p-3">
                                 {tech.image ?
