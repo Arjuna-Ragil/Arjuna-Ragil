fff3db4b97454326d18a02634b8037d040e22b6f
Arjuna <arjunaragilputera@gmail.com>
2025-11-07 17:07:42 +0700

    hotfix: spelling fix

--- src/app/(main)/component/home.js
+++ src/app/(main)/component/home.js
@@ -1,8 +1,10 @@
 export default function Home(){
+    const tagline = `"Scene, Camera, Renderer: The holy trinity of our new dimension"`
+
     return(
         <div className="min-h-screen h-full w-full grid grid-rows-2 p-5" id="content">
             <div className="h-full w-full flex items-center justify-center flex-col gap-3 text-white">
-                <h1 className="text-3xl font-bold text-center">"Scene, Camera, Renderer: The holy trinity of our new dimension"</h1>
+                <h1 className="text-3xl font-bold text-center">{tagline}</h1>
                 <p className="text-xl text-center">Changing Plain Website, To Interactive and Responsive 3D Space</p>
             </div>
         </div>
